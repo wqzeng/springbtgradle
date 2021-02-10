3db63079256cb09d69272e09c11a2c49c8584574
moke.tsang <moke.tsang@vipshop.com>
2021-02-10 10:40:50 +0800

    add StreamTest

--- src/test/java/com/wqzeng/test/Goods.java
+++ src/test/java/com/wqzeng/test/Goods.java
@@ -2,8 +2,23 @@ package com.wqzeng.test;
 
 import lombok.Data;
 
-@Data
 public class Goods {
     private Integer time;
     private String name;
+
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/com/wqzeng/test/StreamTest.java
+++ src/test/java/com/wqzeng/test/StreamTest.java
@@ -1,14 +1,19 @@
 package com.wqzeng.test;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamTest {
+    private Logger logger= LoggerFactory.getLogger(this.getClass());
     @Test
     public void testMap() {
         Integer now=5;
@@ -16,10 +21,23 @@ public class StreamTest {
         for (int i = 0; i <10 ; i++) {
             Goods goods=new Goods();
             goods.setTime(i);
+            goods.setName("test"+i);
+            goodsList.add(goods);
+        }
+        for (int i = 0; i <10 ; i++) {
+            Goods goods=new Goods();
+            goods.setTime(i+1);
+            goods.setName("test"+i);
             goodsList.add(goods);
         }
 
-        Assert.assertEquals(goodsList.size(),10);
+        Map<Integer, Goods> goodsMap = goodsList.stream().filter(goods -> goods.getTime() < now).collect(Collectors.toMap(goods -> goods.getTime(), g -> g, (key1, key2) -> key2));
+        Map<Integer, Goods> goodsMap2 = goodsList.stream().filter(goods -> goods.getTime() >= now).collect(Collectors.toMap(goods -> goods.getTime(), g -> g, (key1, key2) -> key1));
+        Map<Integer, List<Goods>> goodsMap3 = goodsList.stream().collect(Collectors.groupingBy(g->g.getTime()));
+        Assert.assertEquals(goodsList.size(),20);
+        logger.info(JSONObject.toJSONString(goodsMap));
+        logger.info(JSONObject.toJSONString(goodsMap2));
+        logger.info(JSONObject.toJSONString(goodsMap3));
     }
 
 }

66ec3d3e738732fc053b27757737517640fc6e71
zengwqiang <zengwqiang@126.com>
2024-02-02 17:53:30 +0800

    add logicalRegex

--- src/test/java/com/wqzeng/test/StringTest.java
+++ src/test/java/com/wqzeng/test/StringTest.java
@@ -2,13 +2,16 @@ package com.wqzeng.test;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.regex.Pattern;
 
 public class StringTest {
     private static final Pattern TOKENS = Pattern.compile("(\\s+)|(AND)|(OR)|(=)|(\\()|(\\))|(\\w+)|\'([^\']+)\'");
 
     @Test
-    public void splitTest() {
+    public void testSplit() {
         String str="Chr in (chrM) and Mutant Frequency>=5% and Mutant Reads>150";
         String[] array=str.split("and");
         for (int i = 0; i < array.length; i++) {
@@ -23,10 +26,24 @@ public class StringTest {
         }
     }
     @Test
-    public void indexTest() {
+    public void testIndexOf() {
         String str="Gender";
         String name="gender";
         System.out.println(str.toLowerCase().indexOf(name));
+    }
+    @Test
+    public void testFormat () {
+        DecimalFormat formatter = new DecimalFormat("0.#E0");
+        String str = "32";
+        //判断str是否可以转换成BigDecimal
+        boolean flag = str.matches("[+-]?(\\d+(\\.?\\d*)?|\\.\\d+)([Ee][+-]?\\d+)?");
+        if (flag) {
+            BigDecimal decimal=new BigDecimal(str);
+//            System.out.println(decimal.setScale(2,BigDecimal.ROUND_HALF_UP));
+            String de= decimal.multiply(new BigDecimal(100)).setScale(2, RoundingMode.HALF_UP).toString()+"%";
+            System.out.println(de);
+            System.out.printf(formatter.format(decimal.setScale(3,BigDecimal.ROUND_HALF_UP)));
+        }
     }
 
 }
